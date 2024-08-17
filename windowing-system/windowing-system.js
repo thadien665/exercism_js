20f486439f73c9f5b7e135716575177ead175ee4
thadien665 <marcinlejman665@gmail.com>
2024-08-17 18:36:28 +0200

    all tests OK

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -30,4 +30,46 @@ export class ProgramWindow {
         this.size = new Size();
         this.position = new Position();
     }
+
+    resize(Size) {
+        if (Size.width < 1) {
+            Size.width = 1;
+        }
+        if (Size.height < 1) {
+            Size.height = 1;
+        }
+        if (Size.width > this.screenSize.width - this.position.x) {
+            Size.width = this.screenSize.width - this.position.x;
+        }
+        if (Size.height > this.screenSize.height - this.position.y) {
+            Size.height = this.screenSize.height - this.position.y;
+        } 
+        this.size.width = Size.width;
+        this.size.height = Size.height;
+    }
+
+    move(Position) {
+        if (Position.x < 0) {
+            Position.x = 0;
+        }
+        if (Position.y < 0) {
+            Position.y = 0;
+        }
+        if (Position.x + this.size.width > this.screenSize.width ) {
+            Position.x = this.screenSize.width - this.size.width;
+        }
+        if (Position.y + this.size.height > this.screenSize.height) {
+            Position.y = this.screenSize.height - this.size.width;
+        }
+        this.position.x = Position.x;
+        this.position.y = Position.y;
+    }
+}
+
+export function changeWindow(ProgramWindow) {
+    ProgramWindow.size.width = 400;
+    ProgramWindow.size.height = 300;
+    ProgramWindow.position.x = 100;
+    ProgramWindow.position.y = 150;
+    return ProgramWindow;
 }
