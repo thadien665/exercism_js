8dca5f89c535b9be9567c57c2e1f44eda85bd80f
thadien665 <marcinlejman665@gmail.com>
2024-08-17 13:15:11 +0200

    8/17 tests OK

---
+++ windowing-system/windowing-system.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+/**
+ * Implement the classes etc. that are needed to solve the
+ * exercise in this file. Do not forget to export the entities
+ * you defined so they are available for the tests.
+ */
+export function Size(width = 80, height = 60){
+    this.width = width;
+    this.height = height;
+    
+}
+Size.prototype.resize = function(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+}
+
+export function Position(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+}
+Position.prototype.move = function(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+}
+
+export class ProgramWindow {
+    constructor(screenSize, size, position) {
+        this.screenSize = new Size(800, 600);
+        this.size = new Size();
+        this.position = new Position();
+    }
+}
